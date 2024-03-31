9bb054f0fbee32ebc186ed9080cdc29c0703c6c6
Oussama Moussaoui <omoussaoui1337@gmail.com>
2024-03-31 22:29:13 +0000

    chore: use const imports

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-var {
+const {
   linearExpo: exp1,
   binExpoRecursive: exp2,
   binExpoIterative: exp3,
   getRandomArgsCb,
 } = require('./functions');
-var { testResults } = require('./testResults');
-var { benchmark } = require('./benchmark');
+const { testResults } = require('./testResults');
+const { benchmark } = require('./benchmark');
 
 // test functions
 for (let i = 0; i < 1000; i++) {
